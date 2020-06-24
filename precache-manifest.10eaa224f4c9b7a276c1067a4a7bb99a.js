6f938fc3ddf5b686f68d5d172e021020a96c488e
Mahmoud-farargy <56843174+Mahmoud-farargy@users.noreply.github.com>
2020-06-24 14:43:37 +0200

    Add files via upload

---
+++ precache-manifest.10eaa224f4c9b7a276c1067a4a7bb99a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3e5fe075ce814b9327df56be16c7dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ab65134d52be2b423bf",
+    "url": "/static/css/main.1b82f915.chunk.css"
+  },
+  {
+    "revision": "0e184565a369501c9ea4",
+    "url": "/static/js/2.093d2fd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.093d2fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab65134d52be2b423bf",
+    "url": "/static/js/main.5e86e4de.chunk.js"
+  },
+  {
+    "revision": "10de6a062ce0d80a5240",
+    "url": "/static/js/runtime-main.0d656b8b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
